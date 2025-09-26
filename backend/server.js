25fc21a669c0f48f8e188e7577130d1299167c31
Ritesh Kumar <riteshcodewilatechnology@gmail.com>
2025-09-26 21:23:04 +0530

    server is ready and backend setup folder structring

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+})
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000");
+})
