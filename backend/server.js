6c317a9b10324c504580bfc3351c0b8f138acc04
Ritesh Kumar <riteshcodewilatechnology@gmail.com>
2025-10-01 14:33:31 +0530

    crud api implemented

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './modules/product.module.js'; // <-- apna model import karo
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
@@ -36,7 +37,47 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server error" });
   }
 });
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  console.log("id", id);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid product id " });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
 
+    res.status(200).json({ success: true, data: updatedProduct });
+
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" })
+
+
+  }
+})
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("id", id);
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted successfully" });
+
+  } catch (error) {
+    console.error("Error in deleting product:", error.message);
+    res.status(404).json({ success: false, message: "Product Not Found With this id " });
+
+  }
+})
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error in fetching product:", error.message);
+    res.status(500).json({ success: false, message: "Server error" })
+
+  }
+})
 app.listen(5000, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
